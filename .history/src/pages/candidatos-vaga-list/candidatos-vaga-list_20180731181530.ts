99b48b40c5f3e5964664930e44082426af996c25
jose <jrobertustorres@gmail.com>
2018-07-31 19:12:01 -0300

    correções e melhorias

---
+++ .history/src/pages/candidatos-vaga-list/candidatos-vaga-list_20180731181530.ts
@@ -0,0 +1,194 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { Constants } from '../../app/constants';
+
+//I18N
+import { TranslateService } from '@ngx-translate/core';
+import { availableLanguages, sysOptions } from '../i18n/i18n-constants';
+
+//SERVICES
+import { VagaService } from '../../providers/vaga-service';
+
+//ENTITYS
+import { VagaListaEntity } from '../../model/vaga-lista-entity';
+import { VagaDetalheEntity } from '../../model/vaga-detalhe-entity';
+
+//PAGES
+import { DetalheCandidatoVagaPage } from '../detalhe-candidato-vaga/detalhe-candidato-vaga';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-candidatos-vaga-list',
+  templateUrl: 'candidatos-vaga-list.html',
+})
+export class CandidatosVagaListPage {
+  segment: string = "candidatosList"; // default button
+  public idVaga: number;
+  public qtdUsuariosVaga: number;
+  public qtdFinalistaVaga: number;
+  private nomeVaga: string;
+  private vagas;
+  private finalistasVagas;
+  languages = availableLanguages;
+  private selectedLanguage = null;
+  private translate: TranslateService;
+  private _idioma: string;
+  private loadingText: string;
+  private loading = null;
+  private vagaDetalheEntity: VagaDetalheEntity;
+  private refresh: boolean = false;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private vagaService: VagaService,
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController,
+              translate: TranslateService) {
+
+    this.translate = translate;
+    this.vagaDetalheEntity = new VagaDetalheEntity();
+    this.idVaga = navParams.get('idVaga');
+    this.nomeVaga = navParams.get('nomeVaga');
+  }
+
+  ngOnInit() {
+    this.getLanguage();
+  }
+
+  ionViewDidLoad() {
+  }
+
+  loadMore(infiniteScroll) {
+
+    setTimeout(() => {
+
+      this.getCandidatosVaga();
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  doRefreshCandidatosVaga(refresher) {
+    // this.refresh = true;
+    this.getCandidatosVaga();
+
+    setTimeout(() => {
+      refresher.complete();
+    }, 2000);
+  }
+
+  selectedTabChanged($event): void {
+    if ($event._value == "candidatosList") {
+        this.getCandidatosVaga();
+        this.refresh = true;
+      } else {
+        this.getFinalistasVaga();
+        this.refresh = false;
+    }
+  }
+
+  getCandidatosVaga() {
+    try {
+      this.vagaDetalheEntity.limiteDados = this.vagaDetalheEntity.limiteDados ? this.vagas.length : null;
+      this.vagaDetalheEntity.idVaga = this.idVaga;
+
+      // if(this.vagaDetalheEntity.limiteDados == null) {
+      if(this.refresh == false) {
+        this.loading = this.loadingCtrl.create({
+          content: this.loadingText
+        });
+        this.loading.present();
+      }
+
+      this.vagaService.findUsuarioByIdVaga(this.vagaDetalheEntity)
+        .then((vagasListaEntityResult: VagaListaEntity) => {
+          this.vagas = vagasListaEntityResult;
+          // this.vagas.limiteDados = this.vagas.length;
+          this.vagaDetalheEntity.limiteDados = this.vagas.length;
+
+          console.log(this.vagaDetalheEntity);
+
+          if (this.vagas.length > 0) {
+            this.qtdUsuariosVaga = this.vagas[0].qtdUsuariosVaga;
+            // this.nomeVaga = this.vagas[0].nomeVaga;
+          }
+          this.refresh = true;
+          this.loading ? this.loading.dismiss() : '';
+      }, (err) => {
+        // this.loading.dismiss();
+        this.loading ? this.loading.dismiss() : '';
+        this.alertCtrl.create({
+          subTitle: err.message,
+          buttons: ['OK']
+        }).present();
+      });
+    }
+    catch (err){
+      if(err instanceof RangeError){
+        console.log('out of range');
+      }
+      console.log(err);
+    }
+  }
+
+  getFinalistasVaga() {
+    try {
+
+      this.loading = this.loadingCtrl.create({
+        content: this.loadingText
+      });
+      this.loading.present();
+
+      // this.vagaDetalheEntity = new VagaDetalheEntity();
+      this.vagaDetalheEntity.idVaga = this.idVaga;
+
+      this.vagaService.findFavoritosVaga(this.vagaDetalheEntity)
+        .then((vagasListaEntityResult: VagaListaEntity) => {
+          this.finalistasVagas = vagasListaEntityResult;
+          if (this.vagas.length > 0) {
+            this.qtdFinalistaVaga = this.vagas[0].qtdUsuarioFinalistasVaga;
+            this.nomeVaga = this.vagas[0].nomeVaga;
+          }
+          this.loading.dismiss();
+      }, (err) => {
+        this.loading.dismiss();
+        this.alertCtrl.create({
+          subTitle: err.message,
+          buttons: ['OK']
+        }).present();
+      });
+    }
+    catch (err){
+      if(err instanceof RangeError){
+        console.log('out of range');
+      }
+      console.log(err);
+    }
+  }
+
+  callDetalheCandidatoVaga(idVagaUsuario) {
+    this.navCtrl.push(DetalheCandidatoVagaPage, {
+      idVagaUsuario: idVagaUsuario
+    })
+  }
+
+  getLanguage() {
+    this._idioma = sysOptions.systemLanguage == 'pt-br' ? 'pt-br' : 'en';
+    this.selectedLanguage = localStorage.getItem(Constants.IDIOMA_USUARIO);
+        if(!this.selectedLanguage){
+          this.selectedLanguage = this._idioma;
+        }
+        else if(this.selectedLanguage) {
+          this.selectedLanguage = this.selectedLanguage;
+          if (this.selectedLanguage == 'pt-br') {
+            this.loadingText = 'Aguarde...';
+          } else {
+            this.loadingText = 'Wait...';
+          }
+        }
+        this.getCandidatosVaga();
+        this.translate.use(this.selectedLanguage);
+
+  }
+
+}
---
+++ .history/src/pages/principal/principal_20180731174718.ts
@@ -0,0 +1,225 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, PopoverController, ModalController } from 'ionic-angular';
+import { Constants } from '../../app/constants';
+
+//I18N
+import { TranslateService } from '@ngx-translate/core';
+import { availableLanguages, sysOptions } from '../i18n/i18n-constants';
+
+//SERVICES
+import { VagaService } from '../../providers/vaga-service';
+
+//ENTITYS
+import { VagaListaEntity } from '../../model/vaga-lista-entity';
+import { VagaDetalheEntity } from '../../model/vaga-detalhe-entity';
+
+//PAGES
+import { CriaVagaPage } from './../cria-vaga/cria-vaga';
+import { ModalFiltroPage } from '../modal-filtro/modal-filtro';
+import { CandidatosVagaListPage } from '../candidatos-vaga-list/candidatos-vaga-list';
+
+@IonicPage()
+@Component({
+  selector: 'page-principal',
+  templateUrl: 'principal.html',
+})
+export class PrincipalPage {
+  // segment: string = "vagasAtivas"; // default button
+
+  languages = availableLanguages;
+  private selectedLanguage = null;
+  private translate: TranslateService;
+  private vagasAtivas;
+  // private vagasArquivadas;
+  // private vagas;
+  private loadingText: string;
+  private loading = null;
+  private vagaDetalheEntity: VagaDetalheEntity;
+  private vagaListaEntity: VagaListaEntity;
+  private _idioma: string;
+  public qtdVagasAtivas: number;
+  private refresh: boolean = false
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              translate: TranslateService,
+              private vagaService: VagaService,
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController,
+              public modalCtrl: ModalController,
+              public popoverCtrl: PopoverController) {
+
+    this.translate = translate;
+    this.vagaDetalheEntity = new VagaDetalheEntity();
+    this.vagaListaEntity = new VagaListaEntity();
+
+  }
+
+  ngOnInit() {
+    this.getLanguage();
+    this.getVagasAtivas(this.vagaDetalheEntity);
+  }
+
+  ionViewDidLoad() {
+  }
+
+  loadMore(infiniteScroll) {
+
+    setTimeout(() => {
+
+      this.getVagasAtivas(this.vagaDetalheEntity);
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  doRefreshVagasAtivas(refresher) {
+    // this.vagaDetalheEntity = new VagaDetalheEntity();
+    this.getVagasAtivas(this.vagaDetalheEntity);
+
+    setTimeout(() => {
+      refresher.complete();
+    }, 2000);
+  }
+
+  // selectedTabChanged($event): void {
+  //   if ($event._value == "vagasAtivas") {
+  //       this.getVagasAtivas(this.vagaDetalheEntity);
+  //     } else {
+  //         this.getVagasArquivadas();
+  //   }
+  // }
+
+  // presentPopover(myEvent, idVaga) {
+  //   let popover = this.popoverCtrl.create(PopoverPrincipalPage, {
+  //     idVaga: idVaga
+  //   });
+  //   popover.present({
+  //     ev: myEvent
+  //   });
+  //   popover.onDidDismiss(data => {
+  //   });
+  // }
+
+  openModalFiltro(){
+    let modal = this.modalCtrl.create(ModalFiltroPage);
+
+    modal.onDidDismiss((data) => {
+      if (data) {
+        // this.segment = "vagasAtivas";
+        this.getVagasAtivas(data.filter);
+      }
+    });
+
+    modal.present();
+  }
+
+  // getVagasArquivadas() {
+  //   try {
+  //     this.vagaDetalheEntity.limiteDados = this.vagaDetalheEntity.limiteDados ? this.vagasArquivadas.length : null;
+  //     console.log(this.segment);
+
+  //     this.loading = this.loadingCtrl.create({
+  //       content: this.loadingText
+  //     });
+  //     this.loading.present();
+
+  //     this.vagaService.findVagaFinalizadasFornecedorByVaga(this.vagaDetalheEntity)
+  //       .then((vagasListaEntityResult: VagaListaEntity) => {
+  //         this.vagasArquivadas = vagasListaEntityResult;
+  //         this.vagasArquivadas.limiteDados = this.vagasArquivadas.length;
+
+  //         console.log(this.vagasArquivadas);
+
+  //         this.loading.dismiss();
+  //     }, (err) => {
+  //       this.loading.dismiss();
+  //       this.alertCtrl.create({
+  //         subTitle: err.message,
+  //         buttons: ['OK']
+  //       }).present();
+  //     });
+  //   }
+  //   catch (err){
+  //     if(err instanceof RangeError){
+  //       console.log('out of range');
+  //     }
+  //     console.log(err);
+  //   }
+  // }
+
+  getVagasAtivas(filtro) {
+    try {
+      if(this.vagaDetalheEntity.limiteDados == null) {
+        this.loading = this.loadingCtrl.create({
+          content: this.loadingText
+        });
+        this.loading.present();
+      }
+
+      this.vagaDetalheEntity = filtro;
+
+      this.vagaService.findVagaFornecedorByVaga(this.vagaDetalheEntity)
+        .then((vagasListaEntityResult: VagaListaEntity) => {
+          this.vagasAtivas = vagasListaEntityResult;
+
+          this.qtdVagasAtivas = this.vagasAtivas.length
+
+          this.refresh = true;
+          this.loading ? this.loading.dismiss() : '';
+          // this.loading.dismiss();
+      }, (err) => {
+        this.loading ? this.loading.dismiss() : '';
+        this.alertCtrl.create({
+          subTitle: err.message,
+          buttons: ['OK']
+        }).present();
+      });
+    }
+    catch (err){
+      if(err instanceof RangeError){
+        console.log('out of range');
+      }
+      console.log(err);
+    }
+  }
+
+  callTelaCriarVaga() {
+    this.navCtrl.push(CriaVagaPage);
+  }
+
+  callTelaCriarVagaByIdVaga(idVaga) {
+    this.navCtrl.push(CriaVagaPage, {
+      idVaga: idVaga
+    })
+  }
+
+  callCandidatosVaga(idVaga, nomeVaga) {
+    this.navCtrl.push(CandidatosVagaListPage, {
+      idVaga: idVaga,
+      nomeVaga: nomeVaga
+    })
+  }
+
+  getLanguage() {
+    this._idioma = sysOptions.systemLanguage == 'pt-br' ? 'pt-br' : 'en';
+    this.selectedLanguage = localStorage.getItem(Constants.IDIOMA_USUARIO);
+    // this._storage.get('selectedLanguage').then((selectedLanguage) => {
+        if(!this.selectedLanguage){
+          this.selectedLanguage = this._idioma;
+        }
+        else if(this.selectedLanguage) {
+          this.selectedLanguage = this.selectedLanguage;
+          if (this.selectedLanguage == 'pt-br') {
+            this.loadingText = 'Procurando vagas...';
+          } else {
+            this.loadingText = 'Looking for vacancies...';
+          }
+        }
+        // this.getVagasDestaquePrincipal();
+        this.translate.use(this.selectedLanguage);
+
+      // });
+
+  }
+
+}
